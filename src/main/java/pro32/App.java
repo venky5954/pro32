43168a59a540a65466ce42e0e382385368cda6ec
lucky <venkateshv014@gmail.com>
2020-01-24 18:37:38 +0530

    pro32

---
+++ src/main/java/pro32/App.java
@@ -0,0 +1,10 @@
+package pro32;
+
+public class App {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
